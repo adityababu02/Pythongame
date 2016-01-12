81cfc150f78d7a3962bd0fa00004b6a629d14d79
RajaAnthati <nagaiik@gmail.com>
2016-01-12 21:01:35 +0000

    implemented discount functionality

--- FruitShop/src/com/test/ApplyDiscount.scala
+++ FruitShop/src/com/test/ApplyDiscount.scala
@@ -12,15 +12,31 @@ class ApplyDiscount(var apples:Int,var oranges:Int) {
   def price:Double={
     
     var sum:Double=0;
-    
-    if(oranges>0){
+    if(oranges==1 || oranges==2){
       
-      if(oranges>=3)
-      sum+=0.66*oranges*0.25
-      else 
-        sum+=0.25*oranges
+      sum+=oranges*0.25
     }
+    
+    else if(oranges>2){
+      
+      sum+=(oranges/3)*0.25+(oranges%3)*0.25
+   
+      
+    }
+    
+    if(apples>=2){
+      
+      if(apples%2==0)
+        sum+=0.50*apples*0.60
+      else 
+        sum+=0.50*apples*0.60+0.60   // add an extra apple if odd number 
+    }
+     
+      
+      
+    
     0.0
+    
   } 
   
 }
